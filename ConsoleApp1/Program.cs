9e2ce97c80e13f7e47e3e49adf7a63be60f9976a
Melaka36 <melaka36pathiranagama@gmail.com>
2023-06-01 19:31:56 +0530

    type convertions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 4;
+            float Number = 23.32f;
+            Console.WriteLine(Number);
+
+            //format String 
+            Console.WriteLine("{0} {1}",byte.MinValue,byte.MaxValue);
+
+            //implycit type conversion
+            byte b = 1; // 1 byte
+            int i = b;  // 4 bytes
+            Console.WriteLine(i);
+
+            //Explicit type conversion
+
+            int j = 3;
+            byte c = (byte)j;  // j=3 , 3 can be stored in a byte so we can cast
+            Console.WriteLine(c);
+
+            int k = 30230;
+            byte d = (byte)k;  // j=300 , 3 cannot be stored in a byte so we can cast
+            Console.WriteLine(d); //data loss
+        }
+    }
+}
